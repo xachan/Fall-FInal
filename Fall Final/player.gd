ee17722b5c93b05b2d6d24be82f00c74ee769087
xachan <121971475+xachan@users.noreply.github.com>
2023-12-19 11:41:31 -0800

    enemy scene complete

--- Fall Final/player.gd
+++ Fall Final/player.gd
@@ -1,27 +1,36 @@
 extends Node2D
 
+@export var speed = 400
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 	
-
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("right"):
+		velocity.x += 1
+	if Input.is_action_pressed("down"):
+		velocity.y += 1
+	if Input.is_action_pressed("left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("up"):
+		velocity.y -= 1
+	
+	
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+	
+		position += velocity * delta
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var velocity = Vector2.ZERO
-	if Input.is_action_pressed("move_right"):
-		velocity.x += 1
-	if Input.is_action_pressed("move_down"):
-		velocity.y += 1
-	if Input.is_action_pressed("move_left"):
-		velocity.x -= 1
-	if Input.is_action_pressed("move_up"):
-		velocity.y -= 1
 	
 
+		
 
-func _on_body_entered(body):
+
 	
+
 
 
