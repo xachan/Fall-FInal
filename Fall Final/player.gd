55fab8db3b3613083eed0495a30bbf76d9ff8160
xachan <121971475+xachan@users.noreply.github.com>
2023-12-19 12:08:53 -0800

    Set up collision

--- Fall Final/player.gd
+++ Fall Final/player.gd
@@ -1,11 +1,19 @@
-extends Node2D
+extends Area2D
 
-@export var speed = 400
+
+@export var speed = 100
+
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
+	
+	
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
 	
 	var velocity = Vector2.ZERO
 	if Input.is_action_pressed("right"):
@@ -17,20 +25,14 @@ func _ready():
 	if Input.is_action_pressed("up"):
 		velocity.y -= 1
 	
-	
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
-	
 		position += velocity * delta
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	
-
-		
-
-
 	
 
 
 
+
+func _on_body_entered(body):
+	pass # Replace with function body.
+	
